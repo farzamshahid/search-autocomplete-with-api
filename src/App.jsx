e9dfc2c6596cceb19d5ed543d971deefcb93d78f
farzamshahid <farzamshahid6@gmail.com>
2024-03-27 13:28:14 -0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import SearchAutocomplete from './search-auto-complete-api'
+
+function App() {
+
+  return (
+    <>
+      <SearchAutocomplete />
+    </>
+  )
+}
+
+export default App
